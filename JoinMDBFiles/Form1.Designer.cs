2feb2f325157fef0bfaad6b317cfbef127211b41
jdelcabr <jdelcabr@LIM-G18HHS3>
2024-03-26 01:35:16 -0500

    Agregar archivos de proyecto.

---
+++ JoinMDBFiles/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace JoinMDBFiles {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOriginFile = new System.Windows.Forms.TextBox();
+            this.txtTargetFile = new System.Windows.Forms.TextBox();
+            this.btnJoinMDBs = new System.Windows.Forms.Button();
+            this.btnOriginFile = new System.Windows.Forms.Button();
+            this.btnTargetFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archivo de Origen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Archivo de Destino:";
+            // 
+            // txtOriginFile
+            // 
+            this.txtOriginFile.Location = new System.Drawing.Point(155, 25);
+            this.txtOriginFile.Name = "txtOriginFile";
+            this.txtOriginFile.Size = new System.Drawing.Size(262, 22);
+            this.txtOriginFile.TabIndex = 2;
+            // 
+            // txtTargetFile
+            // 
+            this.txtTargetFile.Location = new System.Drawing.Point(155, 70);
+            this.txtTargetFile.Name = "txtTargetFile";
+            this.txtTargetFile.Size = new System.Drawing.Size(262, 22);
+            this.txtTargetFile.TabIndex = 3;
+            // 
+            // btnJoinMDBs
+            // 
+            this.btnJoinMDBs.Location = new System.Drawing.Point(231, 110);
+            this.btnJoinMDBs.Name = "btnJoinMDBs";
+            this.btnJoinMDBs.Size = new System.Drawing.Size(109, 33);
+            this.btnJoinMDBs.TabIndex = 4;
+            this.btnJoinMDBs.Text = "Unir MDB\'s";
+            this.btnJoinMDBs.UseVisualStyleBackColor = true;
+            this.btnJoinMDBs.Click += new System.EventHandler(this.btnJoinMDBs_Click);
+            // 
+            // btnOriginFile
+            // 
+            this.btnOriginFile.Location = new System.Drawing.Point(423, 24);
+            this.btnOriginFile.Name = "btnOriginFile";
+            this.btnOriginFile.Size = new System.Drawing.Size(87, 24);
+            this.btnOriginFile.TabIndex = 5;
+            this.btnOriginFile.Text = "Examinar";
+            this.btnOriginFile.UseVisualStyleBackColor = true;
+            this.btnOriginFile.Click += new System.EventHandler(this.btnOriginFile_Click);
+            // 
+            // btnTargetFile
+            // 
+            this.btnTargetFile.Location = new System.Drawing.Point(423, 70);
+            this.btnTargetFile.Name = "btnTargetFile";
+            this.btnTargetFile.Size = new System.Drawing.Size(87, 23);
+            this.btnTargetFile.TabIndex = 6;
+            this.btnTargetFile.Text = "Examinar";
+            this.btnTargetFile.UseVisualStyleBackColor = true;
+            this.btnTargetFile.Click += new System.EventHandler(this.btnTargetFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(560, 165);
+            this.Controls.Add(this.btnTargetFile);
+            this.Controls.Add(this.btnOriginFile);
+            this.Controls.Add(this.btnJoinMDBs);
+            this.Controls.Add(this.txtTargetFile);
+            this.Controls.Add(this.txtOriginFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".: Join MDB :.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOriginFile;
+        private System.Windows.Forms.TextBox txtTargetFile;
+        private System.Windows.Forms.Button btnJoinMDBs;
+        private System.Windows.Forms.Button btnOriginFile;
+        private System.Windows.Forms.Button btnTargetFile;
+    }
+}
+
---
+++ JoinMDBFiles/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace JoinMDBFiles {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnOriginFile_Click(object sender, EventArgs e) {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.Title = "Seleccionar Archivo MDB";
+                openFileDialog.Filter = "Archivos MDB (*.mdb)|*.mdb|Todos los archivos (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    // El usuario ha seleccionado un archivo, puedes obtener la ruta con openFileDialog.FileName
+                    string rutaArchivoSeleccionado = openFileDialog.FileName;
+                    // Hacer algo con la ruta del archivo seleccionado, por ejemplo, asignarla a un cuadro de texto
+                    txtOriginFile.Text = rutaArchivoSeleccionado;
+                }
+            }
+        }
+
+        private void btnTargetFile_Click(object sender, EventArgs e) {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.Title = "Seleccionar Archivo MDB";
+                openFileDialog.Filter = "Archivos MDB (*.mdb)|*.mdb|Todos los archivos (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    // El usuario ha seleccionado un archivo, puedes obtener la ruta con openFileDialog.FileName
+                    string rutaArchivoSeleccionado = openFileDialog.FileName;
+                    // Hacer algo con la ruta del archivo seleccionado, por ejemplo, asignarla a un cuadro de texto
+                    txtTargetFile.Text = rutaArchivoSeleccionado;
+                }
+            }
+
+        }
+
+        private void btnJoinMDBs_Click(object sender, EventArgs e) {
+            try {
+                if (TextFileValidation()) {
+                    //Se obtiene la ruta de la fuente y del destino
+                    string sourceFilePath = string.Concat(@"", txtOriginFile.Text);
+                    string destinationFilePath = string.Concat(@"", txtTargetFile.Text);
+
+                    //Cadena de conexión para los archivos de base de datos de Access
+                    string sourceConnectionStr = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={sourceFilePath};";
+                    string destinationConnectionStr = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={destinationFilePath};";
+
+                    //Se establecen conexiones a BDD Access
+                    using (OleDbConnection sourceConnection = new OleDbConnection(sourceConnectionStr))
+                    using (OleDbConnection destinationConn = new OleDbConnection(destinationConnectionStr)) {
+
+                        //Abriendo conexión
+                        sourceConnection.Open();
+                        destinationConn.Open();
+
+                        using (ProgressDialog progressDialog = new ProgressDialog()) {
+                            progressDialog.Show();
+                            // Copiar datos de la tabla "Transacciones" de la base de datos de origen a la base de datos de destino
+                            string copyTransaccionesQuery = "INSERT INTO Transacciones IN '" + destinationFilePath + "' SELECT * FROM Transacciones";
+                            using (OleDbCommand copyTransaccionesCommand = new OleDbCommand(copyTransaccionesQuery, sourceConnection)) {
+                                copyTransaccionesCommand.ExecuteNonQuery();
+                            }
+
+                            // Copiar datos de la tabla "Inicializacion" de la base de datos de origen a la base de datos de destino
+                            string copyInicializacionQuery = "INSERT INTO Inicializacion IN '" + destinationFilePath + "' SELECT * FROM Inicializacion";
+                            using (OleDbCommand copyInicializacionCommand = new OleDbCommand(copyInicializacionQuery, sourceConnection)) {
+                                copyInicializacionCommand.ExecuteNonQuery();
+                            }
+                            progressDialog.Close();
+                        }
+
+                        MessageBox.Show("Los archivos MDB se han unido correctamente.", "Operación exitosa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                } else {
+                    MessageBox.Show("El MDB origen/destino no puede ser nulo/vacío", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show($"Verifica que los MDB origen/destino sean correctos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private bool TextFileValidation() {
+            if (txtOriginFile.Text == null || txtOriginFile.Text == string.Empty || txtTargetFile.Text == null || txtTargetFile.Text == string.Empty) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+    }//EndClass
+
+    public class ProgressDialog : Form {
+        public ProgressDialog() {
+            this.Text = "Procesando";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.ControlBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new System.Drawing.Size(200, 100);
+
+            Label label = new Label();
+            label.Text = "Procesando...";
+            label.AutoSize = true;
+            label.Location = new System.Drawing.Point(50, 20);
+
+            ProgressBar progressBar = new ProgressBar();
+            progressBar.Style = ProgressBarStyle.Marquee;
+            progressBar.Size = new System.Drawing.Size(100, 20);
+            progressBar.Location = new System.Drawing.Point(50, 50);
+
+            this.Controls.Add(label);
+            this.Controls.Add(progressBar);
+        }
+    }//EndClass
+
+}//EndNamespace
